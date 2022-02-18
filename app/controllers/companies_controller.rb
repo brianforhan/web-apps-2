bdc0ca1ffb59fd19243926e322fb13b38c1b85c4
brianforhan <brian.forhan@kellogg.northwestern.edu>
2022-02-18 20:28:50 +0000

    commit

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -5,8 +5,17 @@ class CompaniesController < ApplicationController
   end
 
   def show
-    # .find(123) is the same as .where({ id: 123 })[0]
     @company = Company.find(params["id"])
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(params["company"])
+    @company.save
+    redirect_to "/companies"
   end
 
 end
